b09bf07e39e50cf3294941ac8ff577b3c98e9331
joshlwatkins <114649228+joshlwatkins@users.noreply.github.com>
2022-10-10 20:32:09 -0700

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,6 @@
+name = input('What is your name? ')
+print('Hi ' + name)
+color = input('What is your favorite color? ')
+print(name + ' likes ' + color)
+food = input('What is your favorite food? ')
+print(name + ' likes ' + food)
